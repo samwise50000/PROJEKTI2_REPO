3fd8ce5c4d7b5246ec60975656a092a53fdea0f2
samwise50000 <sami.mattila3@metropolia.fi>
2024-03-21 11:29:34 +0200

    First commit

---
+++ src/main/java/Localization.java
@@ -0,0 +1,13 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Localization {
+    public static void main(String[] args) {
+        String lan= "fa";
+        String country = "IR";
+
+        Locale local = new Locale(lan, country);
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("messages", local);
+        System.out.println(resourceBundle.getString("greetings"));
+    }
+}
